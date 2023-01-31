911b1ad2ea7c282eeb2f0b7e20d8289b0beda2b5
Pinto Manuel <pinto@fireworkx.co.za>
2023-01-31 19:06:57 +0200

    Code to be commit

--- script/script.js
+++ script/script.js
@@ -2,3 +2,32 @@ $('#myTab a').click(function (e) {
     e.preventDefault()
     $(this).tab('show')
 })
+
+// Validation function to be called on form submit
+function validateForm() {
+    // Get the form inputs
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    var passwordConfirm = document.getElementById("passwordConfirm").value;
+
+    // Check if email is in correct format
+    var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+    if (!emailReg.test(email)) {
+      alert("Invalid email address");
+      return false;
+    }
+
+    // Check if password is at least 6 characters long
+    if (password.length < 6) {
+      alert("Password must be at least 6 characters long");
+      return false;
+    }
+
+    // Check if password and passwordConfirm match
+    if (password !== passwordConfirm) {
+      alert("Passwords do not match");
+      return false;
+    }
+
+    return true;
+  }
